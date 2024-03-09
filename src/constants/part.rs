4f6fec14c8d3f7b6cf61978591b444c5c2b75f92
Shane Madden <shanemadden@users.noreply.github.com>
2024-03-09 15:04:26 -0700

    rearrange test data

--- src/constants/part.rs
+++ src/constants/part.rs
@@ -36,30 +36,22 @@ pub enum Part {
 extern "C" {
     #[wasm_bindgen(js_name = MOVE)]
     static MOVE_JS: JsString;
-
     #[wasm_bindgen(js_name = WORK)]
     static WORK_JS: JsString;
-
     #[wasm_bindgen(js_name = CARRY)]
     static CARRY_JS: JsString;
-
     #[wasm_bindgen(js_name = ATTACK)]
     static ATTACK_JS: JsString;
-
     #[wasm_bindgen(js_name = RANGED_ATTACK)]
     static RANGED_ATTACK_JS: JsString;
-
     #[wasm_bindgen(js_name = TOUGH)]
     static TOUGH_JS: JsString;
-
     #[wasm_bindgen(js_name = HEAL)]
     static HEAL_JS: JsString;
-
     #[wasm_bindgen(js_name = CLAIM)]
     static CLAIM_JS: JsString;
 }
 
-#[cfg(not(test))]
 thread_local! {
     static PART_NUM_TO_STR_MAP: js_sys::Map = {
         js_sys::Map::new()
@@ -83,34 +75,6 @@ thread_local! {
             .set(&TOUGH_JS, &JsValue::from(Part::Tough as u8))
             .set(&HEAL_JS, &JsValue::from(Part::Heal as u8))
             .set(&CLAIM_JS, &JsValue::from(Part::Claim as u8))
-    };
-}
-
-// test version - use local versions of strings instead of constants
-#[cfg(test)]
-thread_local! {
-    static PART_NUM_TO_STR_MAP: js_sys::Map = {
-        js_sys::Map::new()
-            .set(&JsValue::from(Part::Move as u8), &JsValue::from_str("move"))
-            .set(&JsValue::from(Part::Work as u8), &JsValue::from_str("work"))
-            .set(&JsValue::from(Part::Carry as u8), &JsValue::from_str("carry"))
-            .set(&JsValue::from(Part::Attack as u8), &JsValue::from_str("attack"))
-            .set(&JsValue::from(Part::RangedAttack as u8), &JsValue::from_str("ranged_attack"))
-            .set(&JsValue::from(Part::Tough as u8), &JsValue::from_str("tough"))
-            .set(&JsValue::from(Part::Heal as u8), &JsValue::from_str("heal"))
-            .set(&JsValue::from(Part::Claim as u8), &JsValue::from_str("claim"))
-    };
-
-    static PART_STR_TO_NUM_MAP: js_sys::Map = {
-        js_sys::Map::new()
-            .set(&JsValue::from_str("move"), &JsValue::from(Part::Move as u8))
-            .set(&JsValue::from_str("work"), &JsValue::from(Part::Work as u8))
-            .set(&JsValue::from_str("carry"), &JsValue::from(Part::Carry as u8))
-            .set(&JsValue::from_str("attack"), &JsValue::from(Part::Attack as u8))
-            .set(&JsValue::from_str("ranged_attack"), &JsValue::from(Part::RangedAttack as u8))
-            .set(&JsValue::from_str("tough"), &JsValue::from(Part::Tough as u8))
-            .set(&JsValue::from_str("heal"), &JsValue::from(Part::Heal as u8))
-            .set(&JsValue::from_str("claim"), &JsValue::from(Part::Claim as u8))
     };
 }
 
@@ -151,14 +115,45 @@ impl Part {
 
 #[cfg(test)]
 mod test {
+    use wasm_bindgen::prelude::*;
     use wasm_bindgen_test::*;
 
-    use super::Part;
+    use super::{part_nums_to_str_array, Part};
+
+    thread_local! {
+        static TEST_PART_NUM_TO_STR_MAP: js_sys::Map = {
+            js_sys::Map::new()
+                .set(&JsValue::from(Part::Move as u8), &JsValue::from_str("move"))
+                .set(&JsValue::from(Part::Work as u8), &JsValue::from_str("work"))
+                .set(&JsValue::from(Part::Carry as u8), &JsValue::from_str("carry"))
+                .set(&JsValue::from(Part::Attack as u8), &JsValue::from_str("attack"))
+                .set(&JsValue::from(Part::RangedAttack as u8), &JsValue::from_str("ranged_attack"))
+                .set(&JsValue::from(Part::Tough as u8), &JsValue::from_str("tough"))
+                .set(&JsValue::from(Part::Heal as u8), &JsValue::from_str("heal"))
+                .set(&JsValue::from(Part::Claim as u8), &JsValue::from_str("claim"))
+        };
+
+        static TEST_PART_STR_TO_NUM_MAP: js_sys::Map = {
+            js_sys::Map::new()
+                .set(&JsValue::from_str("move"), &JsValue::from(Part::Move as u8))
+                .set(&JsValue::from_str("work"), &JsValue::from(Part::Work as u8))
+                .set(&JsValue::from_str("carry"), &JsValue::from(Part::Carry as u8))
+                .set(&JsValue::from_str("attack"), &JsValue::from(Part::Attack as u8))
+                .set(&JsValue::from_str("ranged_attack"), &JsValue::from(Part::RangedAttack as u8))
+                .set(&JsValue::from_str("tough"), &JsValue::from(Part::Tough as u8))
+                .set(&JsValue::from_str("heal"), &JsValue::from(Part::Heal as u8))
+                .set(&JsValue::from_str("claim"), &JsValue::from(Part::Claim as u8))
+        };
+    }
 
     #[wasm_bindgen_test]
     pub fn parts_to_array() {
-        let body = [Part::Work, Part::Carry, Part::Move, Part::Move];
-        let array = Part::slice_to_js_array(&body);
+        let body = [Part::Work, Part::Carry, Part::Move, Part::Move].as_slice();
+        let array = TEST_PART_NUM_TO_STR_MAP.with(|map| {
+            // SAFETY: &[Part] contains u8 values because it's repr(u8), safe to transmute
+            // to &[u8]
+            part_nums_to_str_array(map, unsafe { std::mem::transmute(body) })
+        });
         assert_eq!(array.length(), 4);
         assert_eq!(array.get(0), "work");
         assert_eq!(array.get(1), "carry");
