5fa030aebd0b317d649ef77e7c8af56594ef3413
Shane Madden <shanemadden@users.noreply.github.com>
2024-03-09 13:26:16 -0700

    work toward thread_local, change some names up

--- js/part.js
+++ js/part.js
@@ -1,9 +1,9 @@
-module.exports.part_num_for_bodypart = function(const_map, bodypart) {
-    return const_map[bodypart.type]
+module.exports.part_num_for_bodypart = function(part_str_to_num_map, bodypart) {
+    return part_str_to_num_map.get(bodypart.type)
 }
 
-module.exports.part_array_num_to_str = function(const_map, body_num_array) {
+module.exports.part_array_num_to_str = function(part_num_to_str_map, body_num_array) {
     // this is a Uint8Array and its map can't produce strings as-is,
     // spread it first so the map can result in an array with constant strings
-    return [...body_num_array].map((v) => const_map.get(v));
+    return [...body_num_array].map((v) => part_num_to_str_map.get(v));
 }
--- src/constants/convert.rs
+++ src/constants/convert.rs
@@ -3,6 +3,21 @@ use wasm_bindgen::prelude::*;
 
 use crate::{constants::Part, objects::BodyPart};
 
+thread_local! {
+  static PART_NUM_TO_STR_MAP: js_sys::Map = {
+    let map = js_sys::Map::new();
+    map
+      .set(&JsValue::from(Part::Move as u8), &JsValue::from_str("move"))
+      .set(&JsValue::from(Part::Work as u8), &JsValue::from_str("work"))
+      .set(&JsValue::from(Part::Carry as u8), &JsValue::from_str("carry"))
+      .set(&JsValue::from(Part::Attack as u8), &JsValue::from_str("attack"))
+      .set(&JsValue::from(Part::RangedAttack as u8), &JsValue::from_str("ranged_attack"))
+      .set(&JsValue::from(Part::Tough as u8), &JsValue::from_str("tough"))
+      .set(&JsValue::from(Part::Heal as u8), &JsValue::from_str("heal"))
+      .set(&JsValue::from(Part::Claim as u8), &JsValue::from_str("claim"))
+  };
+}
+
 #[wasm_bindgen(module = "/js/part.js")]
 extern "C" {
     pub(crate) fn part_num_for_bodypart(map: &Map, body_part: &BodyPart) -> Part;
@@ -12,24 +27,20 @@ extern "C" {
 #[cfg(test)]
 mod test {
     use wasm_bindgen_test::*;
-    use wasm_bindgen::JsValue;
-    use js_sys::Map;
 
-    use super::part_array_num_to_str;
+    use super::{part_array_num_to_str, PART_NUM_TO_STR_MAP};
 
     #[wasm_bindgen_test]
     pub fn parts_to_array() {
         // work, carry, move, move
-        let map = Map::new();
-        map.set(&JsValue::from_f64(0.), &JsValue::from_str("move"));
-        map.set(&JsValue::from_f64(1.), &JsValue::from_str("work"));
-        map.set(&JsValue::from_f64(2.), &JsValue::from_str("carry"));
-        let body = [1, 2, 0, 0];
-        let array = part_array_num_to_str(&map, &body);
-        assert_eq!(array.length(), 4);
-        assert_eq!(array.get(0), "work");
-        assert_eq!(array.get(1), "carry");
-        assert_eq!(array.get(2), "move");
-        assert_eq!(array.get(3), "move");
+        PART_NUM_TO_STR_MAP.with(|map| {
+            let body = [1, 2, 0, 0];
+            let array = part_array_num_to_str(&map, &body);
+            assert_eq!(array.length(), 4);
+            assert_eq!(array.get(0), "work");
+            assert_eq!(array.get(1), "carry");
+            assert_eq!(array.get(2), "move");
+            assert_eq!(array.get(3), "move");
+        })
     }
 }
