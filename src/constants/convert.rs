b6f8c8d0bfd58e32da41726734b4cbc650bead9d
Shane Madden <shanemadden@users.noreply.github.com>
2024-03-09 12:01:34 -0700

    broken test to mess with

--- src/constants/convert.rs
+++ src/constants/convert.rs
@@ -8,3 +8,19 @@ extern "C" {
     pub(crate) fn part_num_for_bodypart(body_part: &BodyPart) -> Part;
     pub(crate) fn part_array_num_to_str(part_array: &[u8]) -> Array;
 }
+
+#[cfg(test)]
+mod test {
+    use wasm_bindgen_test::*;
+
+    use super::part_array_num_to_str;
+
+    #[wasm_bindgen_test]
+    pub fn parts_to_array() {
+        // work, carry, move
+        let body = [2, 1, 0];
+        let array = part_array_num_to_str(&body);
+        assert_eq!(array.get(0), "work");
+        
+    }
+}
