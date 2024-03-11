4cc20028bc751d414e5bc14ff0a3a1ae8e709908
Shane Madden <shanemadden@users.noreply.github.com>
2024-03-10 18:17:48 -0600

    object id

--- js/object_id.js
+++ js/object_id.js
@@ -1,50 +1,26 @@
-module.exports.object_id_into_uint8array = function(id, arr) {
+object_id_into_uint8array = function(id, arr) {
     // passed array is exactly 16 bytes -- 12 for id, 3 blank, 1 for string pad length
     // initialize all of them so that we can assume they're initialized on the rust side
     const padding = id.length;
+    let skip_bytes = 12 - Math.ceil(padding / 2);
 
-    for (let byte = 0; byte <= 12; byte++) {
-        if (byte * 2 + padding < 23) {
-            arr[byte] = 0;
-        } else if (byte * 2 + padding === 23) {
-            arr[byte] = parseInt(id.substr(byte * 2 - padding, 1), 16)
-        } else {
-            console.log("writing", byte)
-            console.log(id.substr(byte * 2 - padding, 2))
-            console.log(parseInt(id.substr(byte * 2 - padding, 2), 16))
-            arr[byte] = parseInt(id.substr(byte * 2 - padding, 2), 16)
-        }
+    for (let byte = 0; byte < skip_bytes; byte++) {
+        arr[byte] = 0;
+    }
+
+    const offset = padding % 2;
+    // if there's an odd number of characters, grab one for the next byte
+    if (offset === 1) {
+        arr[skip_bytes] = parseInt(id.substr(0, 1), 16)
+        skip_bytes++;
+    }
+
+    for (let byte = 0; byte < 12 - skip_bytes; byte++) {
+        arr[byte + skip_bytes] = parseInt(id.substr(byte * 2 + offset, 2), 16)
     }
     
+    arr[12] = 0;
     arr[13] = 0;
     arr[14] = 0;
-    arr[15] = 0;
-    arr[16] = padding;
+    arr[15] = padding;
 }
-
-/*
-object_id_into_uint8array = function(id, arr) {
-    const padding = id.length;
-
-    for (let byte = 0; byte <= 12; byte++) {
-        if (byte * 2 + padding < 23) {
-            arr[byte] = 0;
-        } else if (byte * 2 + padding === 23) {
-            arr[byte] = parseInt(id.substr(byte * 2 - padding, 1), 16)
-        } else {
-            console.log("writing", byte)
-            console.log(id.substr(byte * 2 - padding, 2))
-            console.log(parseInt(id.substr(byte * 2 - padding, 2), 16))
-            arr[byte] = parseInt(id.substr(byte * 2 - padding, 2), 16)
-        }
-    }
-    
-    arr[13] = 0;
-    arr[14] = 0;
-    arr[15] = 0;
-    arr[16] = padding;
-}
-let array = new Uint8Array(16)
-object_id_into_uint8array("12", array)
-console.log(array)
-*/
